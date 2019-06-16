ef105bd2e27639c3e8f6a90b51234ded23f5036e
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-06-17 11:02:57 +1200

    Added logic to winnie fragement for text view widgets

--- app/src/main/java/com/example/memelords/Winnie_fragment.java
+++ app/src/main/java/com/example/memelords/Winnie_fragment.java
@@ -20,8 +20,8 @@ public class Winnie_fragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_winnie,container,false);
 
-        //winnieTV1 = (TextView) view.findViewById(R.id.winnieTV1);
-        //winnieTV2 = (TextView) view.findViewById(R.id.winnieTV2);
+        winnieTV1 = (TextView) view.findViewById(R.id.winnieTV1);
+        winnieTV2 = (TextView) view.findViewById(R.id.winnieTV2);
         winnieET1 = (EditText) view.findViewById(R.id.winneET1);
         winnieET2 = (EditText) view.findViewById(R.id.winneET2);
 
@@ -33,7 +33,7 @@ public class Winnie_fragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                //winnieTV1.setText(winnieET1.getText().toString());
+                winnieTV1.setText(winnieET1.getText().toString());
             }
 
             @Override
@@ -49,7 +49,7 @@ public class Winnie_fragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-               // winnieTV2.setText(winnieET2.getText().toString());
+                winnieTV2.setText(winnieET2.getText().toString());
             }
 
             @Override
