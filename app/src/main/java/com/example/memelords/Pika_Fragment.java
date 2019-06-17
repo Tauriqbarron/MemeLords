c5e8d211b29452d3695b0bcba338e6c145544fe4
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-06-17 14:18:03 +1200

    added on click function to save pika image.

--- app/src/main/java/com/example/memelords/Pika_Fragment.java
+++ app/src/main/java/com/example/memelords/Pika_Fragment.java
@@ -1,17 +1,23 @@
 package com.example.memelords;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
+import android.text.Layout;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,12 +27,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
 
 public class Pika_Fragment extends Fragment {
     EditText line1,line2,subject;
     TextView subjectTV,line1Tv,line2Tv;
     Button btnSave_pika;
     Context context;
+    ConstraintLayout cont;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -39,6 +51,7 @@ public class Pika_Fragment extends Fragment {
         subjectTV = (TextView) view.findViewById(R.id.subjectTV_pika);
         btnSave_pika = (Button) view.findViewById(R.id.btnsave_pika);
         context = view.getContext();
+        cont = (ConstraintLayout)view.findViewById(R.id.con_pika);
         subject.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -89,14 +102,14 @@ public class Pika_Fragment extends Fragment {
         btnSave_pika.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // saveMeme(context,v );
+                File file =
             }
         });
         return view;
     }
 
     private Bitmap getbitmap(View v){
-        Bitmap memeBit = Bitmap.createBitmap(300,300,Bitmap.Config.ARGB_8888);
+        Bitmap memeBit = Bitmap.createBitmap(v.getWidth(),v.getHeight(),Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(memeBit);
 
         Drawable back = v.getBackground();
@@ -109,8 +122,42 @@ public class Pika_Fragment extends Fragment {
         v.draw(canvas);
         return memeBit;
     }
-   // private File saveMeme(Context context, View v){
-       // File pictureFileDr = new File(Environment.g)
-        //return pic;
-   // }
+   private File saveMeme(Context context, View v){
+        File pictureFileDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"MemeLords");
+       if (!pictureFileDir.exists()) {
+           boolean isDirectoryCreated = pictureFileDir.mkdirs();
+           if (!isDirectoryCreated)
+               Log.i("TAG", "Can't create directory to save the image");
+           return null;
+       }
+           String filename = pictureFileDir.getPath() +File.separator+ System.currentTimeMillis()+".jpg";
+           File pictureFile = new File(filename);
+           Bitmap saveMap =getbitmap(v);
+
+           try{
+               pictureFile.createNewFile();
+               FileOutputStream oStream = new FileOutputStream(pictureFile);
+               saveMap.compress(Bitmap.CompressFormat.PNG,100,oStream);
+               oStream.flush();
+               oStream.close();
+           }catch(IOException e){
+               e.printStackTrace();
+               Log.i("TAG", "There was an issue saving the image.");
+           }
+           scanGallery( context,pictureFile.getAbsolutePath());
+
+        return pictureFile;
+    }
+
+    private void scanGallery(Context context, String path){
+        try {
+            MediaScannerConnection.scanFile(context, new String[]{path}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                public void onScanCompleted(String path, Uri uri) {
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("TAG", "There was an issue scanning gallery.");
+        }
+    }
 }
