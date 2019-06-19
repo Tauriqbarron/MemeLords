94e5dd7c41fbb98df5d35fb7237e93837159b411
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-06-19 14:23:25 +1200

    added staggered recycler view to profile fragment and get data method to databasehelper

--- app/src/main/java/com/example/memelords/DatabaseHelper.java
+++ app/src/main/java/com/example/memelords/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.memelords;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -43,5 +44,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }else{
             return true;
         }
+    }
+    public Cursor getData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + Table_Name;
+        Cursor data = db.rawQuery(query,null);
+        return data;
     }
 }
--- app/src/main/java/com/example/memelords/profile_fragment.java
+++ app/src/main/java/com/example/memelords/profile_fragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class profile_fragment extends Fragment {
     View view;
+    private RecyclerView staggeredRv;
+    private StaggeredRecyclerAdapter adapter;
+    private StaggeredGridLayoutManager manager;
     public profile_fragment() {
     }
 
@@ -17,6 +22,18 @@ public class profile_fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.profile_fragment,container,false);
+
+        staggeredRvSetup();
         return view;
+    }
+
+    public void staggeredRvSetup(){
+        staggeredRv = (RecyclerView)view.findViewById(R.id.staggered_recycleview_profile);
+        manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        staggeredRv.setLayoutManager(manager);
+
+
+
+
     }
 }
