054e9f9d0b953321cf732cf5632048bf7fc73611
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-06-22 12:36:14 +1200

    added save function to change my mind fragement

--- app/src/main/java/com/example/memelords/Change_My_Mind_Fragment.java
+++ app/src/main/java/com/example/memelords/Change_My_Mind_Fragment.java
@@ -1,23 +1,39 @@
 package com.example.memelords;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Change_My_Mind_Fragment extends Fragment {
     TextView cmmTV;
     EditText cmmET;
+    Button btnSave;
     Context context;
-
+    ConstraintLayout cont;
+    String newMeme;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,6 +41,8 @@ public class Change_My_Mind_Fragment extends Fragment {
         cmmTV = (TextView)view.findViewById(R.id.cmm_TV);
         cmmET = (EditText)view.findViewById(R.id.cmm_ET);
         context = view.getContext();
+        cont = (ConstraintLayout) view.findViewById(R.id.cont_layout_cmm);
+        btnSave = (Button)view.findViewById(R.id.cmm_btnSave);
         setup();
         return view;
     }
@@ -45,7 +63,64 @@ public class Change_My_Mind_Fragment extends Fragment {
 
             }
         });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File file = saveMeme(context,cont);
+            }
+        });
     }
+    private File saveMeme(Context context, View v){
+        File pictureFileDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"MemeLords");
+        if (!pictureFileDir.exists()) {
+            boolean isDirectoryCreated = pictureFileDir.mkdirs();
+            if (!isDirectoryCreated)
+                Log.i("TAG", "Can't create directory to save the image");
+            return null;
+        }
+        String filename = pictureFileDir.getPath() +File.separator+ System.currentTimeMillis()+".jpg";
+        newMeme = filename;
+        File pictureFile = new File(filename);
+        Bitmap saveMap =getbitmap(v);
 
+        try{
+            pictureFile.createNewFile();
+            FileOutputStream oStream = new FileOutputStream(pictureFile);
+            saveMap.compress(Bitmap.CompressFormat.PNG,100,oStream);
+            oStream.flush();
+            oStream.close();
+        }catch(IOException e){
+            e.printStackTrace();
+            Log.i("TAG", "There was an issue saving the image.");
+        }
+        scanGallery( context,pictureFile.getAbsolutePath());
+        //addData(newMeme);
+        return pictureFile;
+    }
+    private Bitmap getbitmap(View v){
+        Bitmap memeBit = Bitmap.createBitmap(v.getWidth(),v.getHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(memeBit);
+
+        Drawable back = v.getBackground();
+        if (back != null){
+            back.draw(canvas);
+        }
+        else{
+            canvas.drawColor(Color.WHITE);
+        }
+        v.draw(canvas);
+        return memeBit;
+    }
+    private void scanGallery(Context context, String path){
+        try {
+            MediaScannerConnection.scanFile(context, new String[]{path}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                public void onScanCompleted(String path, Uri uri) {
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("TAG", "There was an issue scanning gallery.");
+        }
+    }
 
 }
